934ffadc1e5f34975f27213c49c7d63c777067e5
Jan Zdráhal <jan.zdrahal@avg.com>
2016-01-25 17:42:34 +0100

    test: copying logs - WIP

--- spec/unit/lib/node_starter/starter_spec.rb
+++ spec/unit/lib/node_starter/starter_spec.rb
@@ -3,7 +3,7 @@ describe NodeStarter::Starter do
     NodeStarter::Starter.new('1', {}, nil, 'foo')
   end
 
-  let(:fake_node) { build :node, path: 'bar' }
+  let(:fake_node) { build :node, path: 'bar', build_id: 123 }
 
   describe '#start_node_process' do
     before(:each) do
@@ -39,8 +39,13 @@ describe NodeStarter::Starter do
       allow(Node).to receive(:create!) { fake_node }
       subject.start_node_process
     end
-    it 'copies log file to ???' do
-      fail NotImplementedError
+    it 'copies log file to artifact storage' do
+      expect(Dir).to receive(:mktmpdir) { '/foo' }
+      fake_config = { uss_node: { artifact_storage_path: '/bar'} }
+      expect(NodeStarter).to receive(:config) { fake_config }
+      expected_source = '/foo/debug.log'
+      expected_target = "/bar/#{fake_node.build_id}/debug.log"
+      expect(FileUtils).to receive(:cp).with(expected_source, expected_target)
     end
     it 'deletes working folder after test' do
       allow(Process).to receive(:spawn) { 123 }
