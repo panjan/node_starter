19e00edcb35ae0c737a4bbadc72b6fc1b1fbb27f
Jan Zdráhal <jan.zdrahal@avg.com>
2016-01-26 13:49:02 +0100

    test: refactoring

--- spec/unit/lib/node_starter/starter_spec.rb
+++ spec/unit/lib/node_starter/starter_spec.rb
@@ -10,17 +10,24 @@ describe NodeStarter::Starter do
       allow(NodeStarter::PrepareBinaries).to receive(:write_to)
       allow(NodeStarter::NodeConfigStore).to receive(:write_complete_file)
       allow(NodeStarter::EnqueueDataStore).to receive(:write_to)
+      allow(Process).to receive(:spawn) { 123 }
+      allow(Process).to receive(:wait)
+      allow(Node).to receive(:create!) { fake_node }
+      allow(Dir).to receive(:mktmpdir) { '/tmpdir' }
+      allow(File).to receive(:exist?)
+      allow(FileUtils).to receive(:cp)
+      allow(fake_node).to receive(:save!)
+      NodeStarter.config['uss_node']['logs_storage_path'] = '/logs'
+      NodeStarter.config['node_binary_name'] = 'binary_name'
     end
 
     it 'prepares binaries' do
       expect(NodeStarter::PrepareBinaries).to receive(:write_to)
-      allow(subject).to receive(:start).and_return(0)
       subject.start_node_process
     end
 
     it 'prepares config' do
       expect(NodeStarter::NodeConfigStore).to receive(:write_complete_file)
-      allow(subject).to receive(:start).and_return(0)
       subject.start_node_process
     end
 
@@ -31,8 +38,6 @@ describe NodeStarter::Starter do
     end
 
     it 'creates node record in db' do
-      allow(Process).to receive(:spawn) { 123 }
-      allow(Process).to receive(:wait)
       expect(Node).to receive(:create!) { fake_node }
       subject.start_node_process
     end
@@ -40,17 +45,12 @@ describe NodeStarter::Starter do
     it 'starts and waits for node process' do
       expect(Process).to receive(:spawn) { 123 }
       expect(Process).to receive(:wait)
-      allow(Node).to receive(:create!) { fake_node }
       subject.start_node_process
     end
 
     it 'copies log file to artifact storage' do
-      NodeStarter.config['uss_node']['logs_storage_path'] = '/logs'
-      NodeStarter.config['node_binary_name'] = 'binary_name'
-      allow(Process).to receive(:spawn) { 123 }
-      allow(Process).to receive(:wait)
       expect(Dir).to receive(:mktmpdir) { '/tmpdir' }
-      allow(File).to receive(:exist?) { true }
+      expect(File).to receive(:exist?) { true }
       expected_source = '/tmpdir/debug.log'
       expected_target = "/logs/#{fake_node.build_id}.log"
       expect(FileUtils).to receive(:cp).with(expected_source, expected_target)
@@ -58,8 +58,6 @@ describe NodeStarter::Starter do
     end
 
     it 'deletes working folder after test' do
-      allow(Process).to receive(:spawn) { 123 }
-      allow(Process).to receive(:wait)
       expected_dir = 'foo/bar'
       expect(Dir).to receive(:mktmpdir) { expected_dir }
       expect(FileUtils).to receive(:rm_rf).with expected_dir
@@ -69,9 +67,7 @@ describe NodeStarter::Starter do
     it 'updates pid in db' do
       expect(fake_node.pid).to be(-1)
       expected_pid = 123
-      allow(Process).to receive(:spawn) { expected_pid }
-      allow(Process).to receive(:wait)
-      expect(Node).to receive(:create!) { fake_node }
+      expect(Process).to receive(:spawn) { expected_pid }
       expect(fake_node).to receive(:save!)
       expect(fake_node).to receive(:save!) do
         expect(fake_node.pid).to eq expected_pid
